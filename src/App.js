8cd54f7aa17c869e25bcbf9e90cc08bc41dd8209
Yesjin <yesjin_nav@naver.com>
2018-09-08 15:15:14 +0900

    Practicing this setState

--- src/App.js
+++ src/App.js
@@ -2,23 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie'
 
-const movies = [
-  {
-    title: "HungerGame",
-    poster: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg",
-  },
-
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg",
-  },
-
-  {
-    title: "Star wars",
-    poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/220px-Star_Wars_Logo.svg.png"
-  }
-
-]
 
 
 
@@ -27,13 +10,36 @@ class App extends Component {
 // update componentWillMount() -> shouldcomponentUpdate() -> rnder() -> componentDidUpdate()
 
   state = {
-    greeting: 'Hello'
+    greeting: 'Hello',
+    movies : [
+      {
+        title: "HungerGame",
+        poster: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg",
+      },
+
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Hunger_Games_cover.jpg",
+      },
+
+      {
+        title: "Star wars",
+        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/220px-Star_Wars_Logo.svg.png"
+      }
+
+    ]
   }
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting: 'Hello again!'
+        movies: [
+          ...this.state.movies, // bring old state movies, and then add something.
+          {
+            title: "transpotting",
+            poster: "http://image.cine21.com/IMGDB/poster/2001/0706/large/172352_18"
+          }
+        ]
       })
     }, 2000)
   }
@@ -42,7 +48,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
